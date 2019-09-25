82cc2a023e1db67e57c3515647164b8c5fdca09f
Allan Paul Casilum <allan.paul.casilum@gmail.com>
2019-09-25 12:50:25 +0800

    add panel child

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 $(document).foundation();
+$(document).ready(function(){
+  console.log('asd');
+  $('.tabs-title').on("mouseover", function() {
+      var $this = this;
+      var tab_id = $($this).find('a').attr('href');
+      console.log(tab_id);
+      //$(".tabs-content .tabs-panel").siblings().hide();
+      $(".tabs-content .tabs-panel").siblings().hide();
+      $(".tabs-content .tabs-panel"+tab_id).show();
+  });
+  $('.dropdown .parent-menu').on('mouseover', function(){
+    console.log('xx');
+    var _id = $(this).data('id');
+    $(".tabs-content .tabs-panel").hide();
+    $(".tabs-content .parent-"+_id).show();
+  });
+});
